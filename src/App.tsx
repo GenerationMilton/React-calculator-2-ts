c03671650d7c93488166c64bda6ad0f1f961c49a
Milton Munoz <livemilton@gmail.com>
2025-11-24 22:22:56 -0500

    Init calculator: :sparkles: Add MenuItem and install vscode extensions

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import MenuItem from "./components/MenuItem";
 import { menuItems } from "./data/db"
 
 function App() {
@@ -13,7 +14,7 @@ function App() {
       <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2">
         <div>
           <h2>Menú</h2>
-
+            {menuItems.map( item => (<MenuItem key={item.id} />))}
         </div>
 
         <div>
