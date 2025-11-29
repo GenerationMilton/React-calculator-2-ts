c82b66ad898382ae1af48dd5344b149ea4b9ce0c
Milton Munoz <livemilton@gmail.com>
2025-11-29 05:53:40 -0500

    Init calculator: :sparkles: create a hook and add objects to order

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import MenuItem from "./components/MenuItem";
 import { menuItems } from "./data/db";
+import useOrder from "./hooks/useOrder";
 
 function App() {
-  console.log(menuItems);
+  
+  const { addItem } = useOrder()
 
   return (
     <>
@@ -20,6 +22,7 @@ function App() {
               <MenuItem 
               key={item.id} 
               item={item}
+              addItem={addItem}
               />
             ))}
           </div>
