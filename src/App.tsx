9d8aaf88c3d105782ffcf4ecd2a69f1f483d419e
Milton Munoz <livemilton@gmail.com>
2025-11-29 05:35:15 -0500

    Init calculator: :sparkles: add product to Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,36 @@
 import MenuItem from "./components/MenuItem";
-import { menuItems } from "./data/db"
+import { menuItems } from "./data/db";
 
 function App() {
-
   console.log(menuItems);
 
   return (
     <>
       <header className="bg-teal-400 py-5">
-        <h1 className="text-center text-4xl font-black ">Calculadora de Propinas y Consumo</h1>
+        <h1 className="text-center text-4xl font-black ">
+          Calculadora de Propinas y Consumo
+        </h1>
       </header>
 
       <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2">
-        <div>
-          <h2>Menú</h2>
-            {menuItems.map( item => (<MenuItem key={item.id} />))}
+        <div className="p-5">
+          <h2 className="text-4 font-black">Menú</h2>
+          <div className="space-y-3 mt-10">
+            {menuItems.map((item) => (
+              <MenuItem 
+              key={item.id} 
+              item={item}
+              />
+            ))}
+          </div>
         </div>
 
         <div>
           <h2>Consumo</h2>
-        
         </div>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
