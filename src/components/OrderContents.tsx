e78ec88aeadedf216a0d4ea32ccdf25f10a6efe4
Milton Munoz <livemilton@gmail.com>
2025-11-29 07:16:51 -0500

    Init calculator: :sparkles: Removing items from the order

--- src/components/OrderContents.tsx
+++ src/components/OrderContents.tsx
@@ -1,11 +1,12 @@
 import { formatCurrency } from "../helpers"
-import type { OrderItem } from "../types"
+import type { MenuItem, OrderItem } from "../types"
 
 type OrderContentsProps = {
     order: OrderItem[]
+    removeItem: (id: MenuItem['id'] ) => void
 }
 
-export default function OrderContents({order}: OrderContentsProps) {
+export default function OrderContents({order, removeItem}: OrderContentsProps) {
   return (
     <div>
         <h2 className='font-black text-4xl'>Consumo</h2>
@@ -31,7 +32,8 @@ export default function OrderContents({order}: OrderContentsProps) {
 
                             <button
                                 className="bg-red-600 h-8 w-8 rounded-full text-white font-black"
-                            >
+                                onClick={ () => removeItem(item.id)}
+                            > 
                                 X
                             </button>
                         </div>
